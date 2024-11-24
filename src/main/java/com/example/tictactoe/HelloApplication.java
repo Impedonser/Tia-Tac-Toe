ee2c4651c11c9b77653b2244e9d3ff55c2af3f2d
Impedonser <mozzhorinkirill2006@gmail.com>
2024-11-24 19:58:37 +0500

    Tic Tac Toe

---
+++ src/main/java/com/example/tictactoe/HelloApplication.java
@@ -0,0 +1,26 @@
+package com.example.tictactoe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 366, 288);
+        stage.setTitle("Tic Tac Toe");
+        Image icon = new Image(getClass().getResourceAsStream("/icon/free-icon-noughts-and-crosses-53654.png"));
+        stage.getIcons().add(icon);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
